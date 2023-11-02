4056a85157f0604531e6a0c6858d2362ef196642
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2023-11-02 14:24:08 +0300

    Second commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class CreditPaymentService {
+public class CreditPaymentService {
+    public int calculate(int credit, double procent, int mounths) {
+        double i = procent / 100 / 12; // ежемесячная процентная ставка
+        double annuity = credit * (i + i / (Math.pow(1 + i, mounths) - 1));
+        return (int) annuity;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CreditPaymentService service = new CreditPaymentService();
+        int credit = 1_000_000;
+        double procent = 9.99;
+        int mounths = 12;
+        double annuity =  service.calculate(1_000_000, 9.99,36);
+        System.out.println(annuity);
     }
 }
